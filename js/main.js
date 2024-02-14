c857db5331dc38c01751bd624cdb3a6bc1fa9ee5
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2024-02-14 18:52:37 +0500

    .

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
 ///table /////
 
 const students = JSON.parse(localStorage.getItem("students")) || [];
-// let students = [
-//   {
-//     id: 1,
-//     firstName: "Doe",
-//     lastName: "back",
-//     location: "qoqon",
-//     date: "12-11-2001",
-//   },
-//   {
-//     id: 2,
-//     firstName: "Doe",
-//     lastName: "back",
-//     location: "qoqon",
-//     date: "12-11-2001",
-//   },
-// ];
 
-// console.log(students);
 
-const btnAdd=document.getElementById('btn-add')
+
+const btnAdd = document.getElementById("btn-add");
 
 const studentList = document.getElementById("students_list");
 
@@ -39,20 +23,19 @@ function displayStudents(students) {
     console.log(student);
     str += `
     <tr>
-    <td>${student.id}</td>
-    <td>${student.firstName}</td>
-    <td>${student.lastName}</td>
-    <td>${student.location}</td>
-    <td>${student.date}</td>
-    <td>${student.promotion}</td>
-    <td>${student.typeOfPromotion}</td>
-    <td>${student.salary}</td>
-    <td>${student.isMarried ? "Ha" : "Yo'q"}</td>
+    <td>${student?.id}</td>
+    <td>${student?.firstName}</td>
+    <td>${student?.lastName}</td>
+    <td>${student?.location}</td>
+    <td>${student?.date}</td>
+    <td>${student?.promotion}</td>
+    <td>${student?.typeOfPromotion}</td>
+    <td>${student?.salary}</td>
+    <td>${student?.isMarried ? "Ha" : "Yo'q"}</td>
     <td>
     <button class="btn btn-sm btn-blue">Edit</button>
-    <button class="btn btn-sm btn-danger " onclick="" >Delete</button>
+    <button class="btn btn-sm btn-danger " onclick="deleteStudent(${student?.id})" >Delete</button>
     </td>
-    
     </tr>
     `;
   });
@@ -62,6 +45,7 @@ displayStudents(students);
 
 btnAdd.addEventListener("click", function (e) {
   e.preventDefault();
+  const students = JSON.parse(localStorage.getItem("students")) || [];
   const newStudent = {
     id: students.length + 1,
     firstName: firstName.value,
@@ -77,4 +61,11 @@ btnAdd.addEventListener("click", function (e) {
   localStorage.setItem("students", JSON.stringify(newStudents));
 });
 
-
+function deleteStudent(studentId) {
+  if (confirm("Are you sure you want to delete this student?")) {
+  const students = JSON.parse(localStorage.getItem("students")) || [];
+   let studentDelete=  students.filter((el)=> el?.id !== studentId )
+   displayStudents(studentDelete);
+   localStorage.setItem("students", JSON.stringify(studentDelete));
+  }
+}
